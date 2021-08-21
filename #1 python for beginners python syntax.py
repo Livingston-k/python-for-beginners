f631f20dbd0e62d4d23047138aedc7a52efe83bf
Livingston <kaddulivingston@gmail.com>
2021-08-21 22:32:29 +0300

    python syntax and variables

---
+++ #1 python for beginners python syntax.py
@@ -0,0 +1,38 @@
+x = 5
+p = 10
+z = x + p
+print(z)
+
+u = "Python"
+o = "is great"
+
+combined = u+" "+o
+print(combined)
+
+#a-z A-Z 0-9
+# name
+# Name
+# NAME 
+
+name,job,country = "Livingston","Soft Engineer","Uganda"
+
+print(name)
+print(job)
+print(country)
+
+print("My name is " + name +" and my job is " + job+" and my country is " + country)
+
+f = "Python"
+k = 5
+
+print(f+ str(k))
+
+
+def myFunction():
+    # global c
+    c = "Python"
+    print(c)
+
+myFunction()
+
+print(c)
---
+++ #2 python for beginners variables in python.py
@@ -0,0 +1,7 @@
+#this is an if condition
+if 5> 1:
+    print("yes 5 is greater than 1")
+    # if 4> 0:
+    #     print("yes 4 is greater than 1")     
+
+
